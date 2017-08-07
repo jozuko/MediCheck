7ae3b6d06cf9bc67c77db891a0063d548426dfd1
jozuko <jozuko.dev@gmail.com>
2017-08-08 08:41:48 +0900

    Initial commit

---
+++ app/src/main/java/com/studiojozu/medicheck/MenuActivity.java
@@ -0,0 +1,13 @@
+package com.studiojozu.medicheck;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MenuActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_menu);
+    }
+}
